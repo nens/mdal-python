4021ca3c40f77ee3bdac4f80d359688d91881de4
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-11 11:41:03 +0200

    fix auditwheel stderr redirection in docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,6 +4,6 @@ mkdir extractedwheel
 cd extractedwheel
 cp ../wheel_build/mdal_python*.whl mdal_python_wheel.zip
 unzip mdal_python_wheel.zip
-IGNORELIBS=(auditwheel lddtree ./extractedwheel/mdal/libmdalpython.*.so 2> >(../extract_libs_to_ignore.py))
 cd ..
+IGNORELIBS=$(auditwheel lddtree ./extractedwheel/mdal/libmdalpython.*.so 2>&1 >/dev/null | (./extract_libs_to_ignore.py))
 auditwheel repair --plat linux_x86_64 $IGNORELIBS wheel_build/mdal_python*.whl --wheel-dir /dist
