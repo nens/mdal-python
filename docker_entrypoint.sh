385f1a3bc050db22b0852bca878a2e025ede5e17
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-15 11:43:11 +0200

    simplify docker entrypoint script

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,9 +1,6 @@
 mkdir wheel_build
 python3 -m build --wheel --outdir wheel_build
 mkdir extractedwheel
-cd extractedwheel
-cp ../wheel_build/mdal_python*.whl mdal_python_wheel.zip
-unzip mdal_python_wheel.zip
-cd ..
+unzip wheel_build/mdal_python*.whl -d extractedwheel
 IGNORELIBS=$(auditwheel lddtree ./extractedwheel/mdal/libmdalpython.*.so 2>&1 >/dev/null | (./extract_libs_to_ignore.py))
 auditwheel repair --plat linux_x86_64 $IGNORELIBS wheel_build/mdal_python*.whl --wheel-dir /dist
