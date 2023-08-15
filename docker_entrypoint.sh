b3a4d4d1e80e60b8758cdca9d767cc0eb3f3d5f5
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-15 15:27:22 +0200

    manually link binary to wheel instead of using auditwheel

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,15 @@
 mkdir wheel_build
 python3 -m build --wheel --outdir wheel_build
+cd wheel_build
+WHEEL_NAME=$(find . -name *.whl)
+cd ..
 mkdir extractedwheel
 unzip wheel_build/mdal_python*.whl -d extractedwheel
-IGNORELIBS=$(auditwheel lddtree ./extractedwheel/mdal/libmdalpython.*.so 2>&1 >/dev/null | (./extract_libs_to_ignore.py))
-auditwheel repair --plat linux_x86_64 $IGNORELIBS wheel_build/mdal_python*.whl --wheel-dir /dist
+cd extractedwheel
+mkdir mdal_python.libs
+cp /usr/local/lib/libmdal.so mdal_python.libs
+DISTINFO_DIR=$(find . -name *.dist-info)
+python3 /mdal-python/set_binary_wheel_hash.py mdal_python.libs/libmdal.so $DISTINFO_DIR/RECORD
+patchelf --set-rpath '$ORIGIN'/../mdal_python.libs mdal/libmdalpython*.so
+zip -r $WHEEL_NAME *
+cp $WHEEL_NAME /dist
---
+++ set_binary_wheel_hash.py
@@ -0,0 +1,26 @@
+import hashlib
+import base64
+import argparse
+from pathlib import Path
+
+def add_file_to_record(filepath: Path, recordpath: Path):
+    with filepath.open(mode='rb') as filehandler:
+        filecontents = filehandler.read()
+    hash = base64.urlsafe_b64encode(
+        hashlib.sha256(filecontents).digest()
+    ).decode('latin1').rstrip('=')
+    bytecount = len(filecontents)
+    
+    wheel_path = recordpath.parents[1]  # "grandparent" of the RECORD file is the wheel root path
+    filename = str(filepath.relative_to(wheel_path))
+    with recordpath.open(mode='a', newline="\r\n") as f:
+        f.write(f"{filename},sha256={hash},{bytecount}\n")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+parser.add_argument('recordfile')
+
+args = parser.parse_args()
+filenamepath = Path(args.filename)
+recordfilepath = Path(args.recordfile)
+add_file_to_record(filenamepath, recordfilepath)
