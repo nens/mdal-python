08a6a71a33872aaed18e3fb094a24f7a47301ef5
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-17 14:04:10 +0200

    annotate docker entrypoint script

--- docker/scripts/docker_entrypoint.sh
+++ docker/scripts/docker_entrypoint.sh
@@ -1,21 +1,29 @@
 #! /usr/bin/env sh
 set -e
 
+# copy source code to a temporary directory
 mkdir /mdal-python
 cd /mdal-python
 cp -r /source-code/* .
+# build it in a temporary build directory
 mkdir wheel_build
 python3 -m build --wheel --outdir wheel_build
 cd wheel_build
 WHEEL_NAME=$(find . -name *.whl)
 cd ..
+# extract it for editing
 mkdir extractedwheel
 unzip wheel_build/mdal_python*.whl -d extractedwheel
 cd extractedwheel
 mkdir mdal_python.libs
+# copy in the MDAL binary, libmdal.so
 cp /usr/local/lib/libmdal.so mdal_python.libs
 DISTINFO_DIR=$(find . -name *.dist-info)
+# add the MDAL binary to the pip wheel file record
 python3 /scripts/set_binary_wheel_hash.py mdal_python.libs/libmdal.so $DISTINFO_DIR/RECORD
+# allow the main libmdalpython binary to find the MDAL binary
 patchelf --set-rpath '$ORIGIN'/../mdal_python.libs mdal/libmdalpython*.so
+# zip the edited contents to a new wheel with the same name
 zip -r $WHEEL_NAME *
+# and copy it to the rw mount directory
 cp $WHEEL_NAME /dist
