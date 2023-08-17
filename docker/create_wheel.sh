7e13ad1cf9ee8c6a042755a1d94729d1a414eb2d
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-17 09:51:06 +0200

    handle running docker build script from other directory

--- docker/create_wheel.sh
+++ docker/create_wheel.sh
@@ -1,2 +1,12 @@
+CURRENTDIR=$(basename "$PWD")
+
+if [ $CURRENTDIR != "docker" ]; then
+    if [ $CURRENTDIR = "mdal-python" ]; then
+        cd docker
+    else
+        echo "Could not find mdal-python or docker directory, please cd into one of those and try again."; exit 2
+    fi
+fi
+
 docker build --tag "mdal-python" .
-docker run --rm -v "$(pwd)":/source-code:ro -v "$(pwd)"/dist:/dist mdal-python
+docker run --rm -v "$(pwd)"/..:/source-code:ro -v "$(pwd)"/../dist:/dist mdal-python
