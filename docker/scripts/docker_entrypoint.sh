83aa7842f51e0febf6acd241ad0adba5e57f92d0
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-11-06 10:53:33 +0100

    update package references

--- docker/scripts/docker_entrypoint.sh
+++ docker/scripts/docker_entrypoint.sh
@@ -13,16 +13,17 @@ WHEEL_NAME=$(find . -name *.whl)
 cd ..
 # extract it for editing
 mkdir extractedwheel
-unzip wheel_build/mdal_python*.whl -d extractedwheel
+unzip wheel_build/mdal*.whl -d extractedwheel
 cd extractedwheel
-mkdir mdal_python.libs
+ls
+mkdir mdal.libs
 # copy in the MDAL binary, libmdal.so
-cp /usr/local/lib/libmdal.so mdal_python.libs
+cp /usr/local/lib/libmdal.so mdal.libs
 DISTINFO_DIR=$(find . -name *.dist-info)
 # add the MDAL binary to the pip wheel file record
-python3 /scripts/set_binary_wheel_hash.py mdal_python.libs/libmdal.so $DISTINFO_DIR/RECORD
+python3 /scripts/set_binary_wheel_hash.py mdal.libs/libmdal.so $DISTINFO_DIR/RECORD
 # allow the main libmdalpython binary to find the MDAL binary
-patchelf --set-rpath '$ORIGIN'/../mdal_python.libs mdal/libmdalpython*.so
+patchelf --set-rpath '$ORIGIN'/../mdal.libs mdal/libmdalpython*.so
 # zip the edited contents to a new wheel with the same name
 zip -r $WHEEL_NAME *
 # and copy it to the rw mount directory
